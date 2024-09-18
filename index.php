713018a7a7ca2c53dd1afa6c10168ad7eb697f6e
Angela <147382156+angelalepiten@users.noreply.github.com>
2024-09-18 17:29:22 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
 
 echo"hello world";
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbname = "lepitendb";
+$table_name = "students_table";
+
+$db = "mysql:host=$host;dbname=$dbname";
+
+$conn = new PDO($db,$user,$password);
+$conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+$first_name = "hello3";
+$last_name = "world3";
+$age = "20";
+$gender = "female";
+
+$stmt = $connection->query("SELECT * FROM students_table");
+while($row = $stmt->fetch()){
+    echo $row['id']."-".$row['first_name']."".
+    $row["last_name"]."<br>";
+}
